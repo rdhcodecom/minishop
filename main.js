a88e7f0eafd0a6b331a57c5b9e22f3201fab5f10
creatiper <113648872+creatiper@users.noreply.github.com>
2022-09-22 22:23:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,4 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide'
- });
-
-(function($) {
-
-	"use strict";
-
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-			BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-			Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-			any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
-
-
-	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-	verticalScrolling: true,
-    scrollProperty: ''
-  });
-
-
+ 
 	var fullHeight = function() {
 
 		$('.js-fullheight').css('height', $(window).height());
